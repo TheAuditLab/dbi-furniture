ff3438d6f16fe7d9518979982b06455b5de19a28
dami-theauditlab <dami@theauditlab.com>
2023-02-08 10:41:43 +0000

    updating service page php and scss

--- templates/services_page.php
+++ templates/services_page.php
@@ -8,33 +8,21 @@
     $text_content = get_field('text_content');
     $row_number = get_field('row_number');
 
-    //services accordion
-    $service_1_image = get_field('service_1_image');
-    $service_1_header = get_field('service_1_header');
-    $service_1_text = get_field('service_1_text');
-    $service_2_image = get_field('service_2_image');
-    $service_2_header = get_field('service_2_header');
-    $service_2_text = get_field('service_2_text');
-    $service_3_image = get_field('service_3_image');
-    $service_3_header = get_field('service_3_header');
-    $service_3_text = get_field('service_3_text');
-    $service_4_image = get_field('service_4_image');
-    $service_4_header = get_field('service_4_header');
-    $service_4_text = get_field('service_4_text');
-    $service_5_image = get_field('service_5_image');
-    $service_5_header = get_field('service_5_header');
-    $service_5_text = get_field('service_5_text');
-    $service_6_image = get_field('service_6_image');
-    $service_6_header = get_field('service_6_header');
-    $service_6_text = get_field('service_6_text');
-    $service_7_image = get_field('service_7_image');
-    $service_7_header = get_field('service_7_header');
-    $service_7_text = get_field('service_7_text');
-    $service_8_image = get_field('service_8_image');
-    $service_8_header = get_field('service_8_header');
-    $service_8_text = get_field('service_8_text');
-
-
+    //content row
+    $image_row_1 = get_field('image_row_1');
+    $text_row_1 = get_field('text_row_1');
+    $image_row_2 = get_field('image_row_2');
+    $text_row_2 = get_field('text_row_2');
+    $image_row_3 = get_field('image_row_3');
+    $text_row_3 = get_field('text_row_3');
+    $image_row_4 = get_field('image_row_4');
+    $text_row_4 = get_field('text_row_4');
+    $image_row_5 = get_field('image_row_5');
+    $text_row_5 = get_field('text_row_5');
+    $image_row_6 = get_field('image_row_6');
+    $text_row_6 = get_field('text_row_6');
+    $image_row_7 = get_field('image_row_7');
+    $text_row_7 = get_field('text_row_7');
 
     //contact form
     $contact_title = get_field('contact_title');
@@ -43,7 +31,7 @@
 ?>
 
 	<section id="services_page">
-        <div class="row">
+        <div class="row mb-4">
             <div class="col-12">
                 <div class="hero-image" style="background-image: url('<?php echo $services_hero_image['url']; ?>')">
                     <div class="container">
@@ -55,95 +43,78 @@
                 </div>
             </div>
         </div>
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-                    <div class="accordion">
-                        <div class="accordion__header is-active">
-                            <div>
-                                <img src="<?php echo $service_1_image['url']; ?>">
-                                <h2><?php echo $service_1_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body is-active">
-                            <p><?php echo $service_1_text ?></p>
-                        </div>
-                        <div class="accordion__header">
-                            <div>
-                                <img src="<?php echo $service_2_image['url']; ?>">
-                                <h2><?php echo $service_2_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body">
-                            <p><?php echo $service_2_text ?></p>
-                        </div>
-                        <div class="accordion__header">
-                            <div>
-                                <img src="<?php echo $service_3_image['url']; ?>">
-                                <h2><?php echo $service_3_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body">
-                            <p><?php echo $service_3_text ?></p>
-                        </div>
-                         <div class="accordion__header">
-                            <div>
-                                <img src="<?php echo $service_4_image['url']; ?>">
-                                <h2><?php echo $service_4_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body">
-                            <p><?php echo $service_4_text ?></p>
-                        </div>
-                        <div class="accordion__header">
-                            <div>
-                                <img src="<?php echo $service_5_image['url']; ?>">
-                                <h2><?php echo $service_5_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body">
-                            <p><?php echo $service_5_text ?></p>
-                        </div>
-                        <div class="accordion__header">
-                            <div>
-                                <img src="<?php echo $service_6_image['url']; ?>">
-                                <h2><?php echo $service_6_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body">
-                            <p><?php echo $service_6_text ?></p>
-                        </div>
-                        <div class="accordion__header">
-                            <div>
-                                <img src="<?php echo $service_7_image['url']; ?>">
-                                <h2><?php echo $service_7_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body">
-                            <p><?php echo $service_7_text ?></p>
-                        </div>
-                        <div class="accordion__header">
-                            <div>
-                                <img src="<?php echo $service_8_image['url']; ?>">
-                                <h2><?php echo $service_8_header; ?></h2>
-                            </div>
-                            <span class="accordion__toggle"></span>
-                        </div>
-                        <div class="accordion__body">
-                            <p><?php echo $service_8_text ?></p>
-                        </div>
-                    </div>
-				</div>
-			</div>
-        </div>
+        <?php if ($row_number >= 3) {
+
+            echo '<div class="row">
+                <div class="col-md-6 col-12">
+                    <div class="image_row"  style="background-image: url(' .  $image_row_1['url'] . ')"></div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <div class="text_row">' . $text_row_1 . '</div>
+                </div>
+            </div>
+            <div class="row flex-md-row flex-column-reverse">
+                <div class="col-md-6 col-12">
+                    <div class="text_row">' . $text_row_2 . '</div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <div class="image_row"  style="background-image: url(' .  $image_row_2['url'] . ')"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <div class="image_row"  style="background-image: url(' .  $image_row_3['url'] . ')"></div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <div class="text_row">' .  $text_row_3 . '</div>
+                </div>
+            </div>';
+        }
+        if ($row_number > 3) {
+            echo '<div class="row flex-md-row flex-column-reverse">
+                <div class="col-md-6 col-12">
+                    <div class="text_row">' .  $text_row_4 . '</div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <div class="image_row"  style="background-image: url(' .  $image_row_4['url'] . ')"></div>
+                </div>
+            </div>';
+        }
+        if ($row_number > 4) {
+            echo '<div class="row">
+                <div class="col-md-6 col-12">
+                    <div class="image_row"  style="background-image: url(' .  $image_row_5['url'] . ')"></div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <div class="text_row">' . $text_row_5 . '</div>
+                </div>
+            </div>';
+        }
+        if ($row_number > 5) {
+            echo '<div class="row flex-md-row flex-column-reverse">
+                <div class="col-md-6 col-12">
+                    <div class="text_row">' .  $text_row_6 . '</div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <div class="image_row"  style="background-image: url(' .  $image_row_6['url'] . ')"></div>
+                </div>
+            </div>';
+        }
+        if ($row_number > 6) {
+            echo '<div class="row">
+                <div class="col-md-6 col-12">
+                    <div class="image_row"  style="background-image: url(' .  $image_row_7['url'] . ')"></div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <div class="text_row">' . $text_row_7 . '</div>
+                </div>
+            </div>';
+        }
+        ?>
         
+        
+        
+
         <div class="container">
 			<div class="row">
 				<div class="col-12">
